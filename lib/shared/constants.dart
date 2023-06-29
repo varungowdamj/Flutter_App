73579c0101ab57b7d67882a8fc7b78a62c51491c
Varun Gowda MJ <104577546+varungowdamj@users.noreply.github.com>
2023-06-29 20:00:40 +0530

    Add files via upload

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:104789534118:web:5385ce1b5f9bcfe8905b67";
+  static String apiKey = "AIzaSyCZ-bowIqbPh2dggvedNn-FiUsRVsAuk-U";
+  static String messagingSenderId = "104789534118";
+  static String projectId = "chatappflutter-9be0f";
+  final primaryColor = const Color(0xFFee7b64);
+}
